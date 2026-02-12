20dd671098640cb9255482fddfc708d519492562
mishakishko <myhaylokishko@gmail.com>
2026-02-12 12:53:26 +0200

    Add more UI/UX

--- core/core.js
+++ core/core.js
@@ -1,2 +1,16 @@
-function active() {
+// Функція для відкриття вікна Add student
+function openModal() {
+    const modal = document.getElementById("add-student-modal");
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    const modal = document.getElementById("add-student-modal");
+    modal.style.display = "none";
+}
+
+function createStudent() {
+    const modal = document.getElementById("modal-forms");
+    
+
 }
